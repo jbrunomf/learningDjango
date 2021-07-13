8a1d2d532a3fc5603d7551a685b4a1003da0adf9
João Bruno M. Fernandes <jbrunomf@outlook.com>
2021-07-12 20:53:50 -0400

    Refactor polls/detail view.

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from .models import Question
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+
 # Create your views here.
 
 
 def index(request):
     context = {'latest_question_list': Question.objects.order_by('-pub_date')[:5]}
-    return render(request, template_name='polls/index.html', context=context)
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("Você está visualizando a questão %s.", question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
     response = "Você está visualizando o resultado da questão %s."
     return HttpResponse(response % question_id)
-
